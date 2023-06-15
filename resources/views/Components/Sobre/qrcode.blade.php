76b0680cf413f8b84e163c74b3286f0adf99267b
Elton <elton13cdz@gmail.com>
2023-06-15 18:28:28 -0400

    atualizacoes

--- resources/views/Components/Sobre/qrcode.blade.php
+++ resources/views/Components/Sobre/qrcode.blade.php
@@ -1,14 +1,20 @@
 <section class="qrcode">
-    <h1 id="title-qrcpde"><strong>Doação com QR Code: Transparência e direcionamento de verba para salvar vidas</strong></h1>
-    <div class="row">
-        <div class="col-md-6">
-            <img  src="{{ asset('img/QRCODE.jpg') }}" alt="Imagem" class="img-fluid">
+
+    <div class="row qrcode-content">
+        <h1 id="title-qrcpde"><strong>Doação com QR Code: Transparência e direcionamento de verba para salvar vidas</strong></h1>
+        <div class="col text-center">
+            <img src="{{ asset('img/QRCODE.jpg') }}" alt="Imagem" class="rounded qrcode-img">
         </div>
         <div class="col-md-6">
             <p class="text-content">Na ONG Bartho, valorizamos a confiança e transparência em cada doação recebida. Por isso, estamos sempre em busca de formas inovadoras para que você, nosso doador, saiba exatamente como seu dinheiro está sendo utilizado em prol dos animais..</p>
             <p class="text-content">Agora, com o uso do QR Code, ficou ainda mais fácil acompanhar o destino da sua contribuição. Ao escanear o código, você será direcionado para uma página exclusiva, onde poderá ver detalhadamente para qual animal específico o seu dinheiro foi enviado.</p>
             <p class="text-content">Acreditamos que é fundamental compartilhar com você cada passo da jornada de resgate e reabilitação desses bichinhos. Queremos que você se sinta parte integrante dessa história de superação, amor e compaixão.</p>
-        </div>
-    </div>
-</section>
 
+
+        </div>
+
+
+
+    </div>
+    <img src="{{ asset('img/sobre-nos-img.png') }}" style="width: 100%; margin-top: 50px;" alt="Imagem" class="img-fluid">
+</section>
