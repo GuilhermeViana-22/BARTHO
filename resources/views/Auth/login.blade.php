5d12da1f1451546a8f2b5fd2fd9a818dab7d9363
GuilhermeViana-22 <gguicido.viana@gmail.com>
2024-06-22 18:02:08 -0300

    rechat nao deu certo

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -23,9 +23,7 @@
                     <div class="form-group mt-3">
                         <input type="password" class="form-control" name="password" placeholder="Senha" required>
                     </div>
-                        <div class="row d-flex justify-content-center">
-                            <div class="g-recaptcha" data-sitekey="6Lfg8f4pAAAAALBzaUjcBx03oSd8pEFOgS_KKIA0"></div>
-                        </div>
+
 
                         <div class="form-group mt-3 text-center">
                         <button type="submit" class="btn btn-brown btn-lg btn-block" id="login-button">Logar</button>
@@ -35,28 +33,5 @@
         </div>
     </div>
 
-    <script>
-        $(function() {
-            function rescaleCaptcha() {
-                var width = $('.g-recaptcha').parent().width();
-                var scale;
-                if (width < 302) {
-                    scale = width / 302;
-                } else {
-                    scale = 1.0;
-                }
 
-                $('.g-recaptcha').css('transform', 'scale(' + scale + ')');
-                $('.g-recaptcha').css('-webkit-transform', 'scale(' + scale + ')');
-                $('.g-recaptcha').css('transform-origin', '0 0');
-                $('.g-recaptcha').css('-webkit-transform-origin', '0 0');
-            }
-
-            rescaleCaptcha();
-            $(window).resize(function() {
-                rescaleCaptcha();
-            });
-
-        });
-    </script>
 @endsection
