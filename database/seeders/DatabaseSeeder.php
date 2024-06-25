8ed48a00adb21b49612c251f59747c6f396cc7e7
Guilherme Augusto dos Santos Viana <guilherme.gasv@policiacientifica.sp.gov.br>
2024-06-25 15:56:55 -0300

    ajustes no corpo do email

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,15 +12,15 @@ class DatabaseSeeder extends Seeder
      * @return void
      */
     public function run(){
-        $this->call(UsersTableSeeder::class);
-        $this->call(TiposAnimaisTableSeeder::class);
-        $this->call(ModulosTableSeeder::class);
-        $this->call(PermissoesTableSeeder::class);
-        $this->call(UsersPermissoesTableSeeder::class);
-        $this->call(CidadesTableSeeder::class);
-        $this->call(SituacoesTableSeeder::class);
-        $this->call(SexoAnimaisTableSeeder::class);
-        $this->call(TiposPerguntasTableSeeder::class);
-        $this->call(TipoPorteTableSeeder::class);
+//        $this->call(UsersTableSeeder::class);
+//        $this->call(TiposAnimaisTableSeeder::class);
+//        $this->call(ModulosTableSeeder::class);
+//        $this->call(PermissoesTableSeeder::class);
+//        $this->call(UsersPermissoesTableSeeder::class);
+//        $this->call(CidadesTableSeeder::class);
+//        $this->call(SituacoesTableSeeder::class);
+//        $this->call(SexoAnimaisTableSeeder::class);
+//        $this->call(TiposPerguntasTableSeeder::class);
+//        $this->call(TipoPorteTableSeeder::class);
     }
 }
